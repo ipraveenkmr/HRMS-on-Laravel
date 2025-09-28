4794e4f75e61fcbbf5b12d1808fd8719ae404364
Praveen <devpraveenkr@gmail.com>
2025-09-28 15:23:20 +0530

    Logo and backend URL updated

--- FLUTTER-APP/lib/constants.dart
+++ FLUTTER-APP/lib/constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   static const String appTitle = 'CodingMSTR';
-  static const String apiLink = 'https://hrms-backend.codingmstr.com/api/';
+  static const String apiLink = 'https://hrms-backend.trickuweb.com/api/';
 }
